7146ae77e5ca26fff3e65cd1ddfb46f96e3aab0c
Christopher Fleetwood <fleetwoodpersonal@gmail.com>
2021-01-21 22:48:06 +0000

    adding prints

--- src/transformers/models/t5/modeling_t5.py
+++ src/transformers/models/t5/modeling_t5.py
@@ -280,6 +280,7 @@ class T5DenseGatedGeluDense(nn.Module):
         hidden_states = self.dropout(hidden_states)
         if hidden_states.dtype == torch.float16:
             if torch.isinf(hidden_states).any() or torch.isnan(hidden_states).any():
+                print('Clamping inside GELU')
                 clamp_value = torch.finfo(hidden_states.dtype).max - 1024
                 hidden_states = torch.clamp(hidden_states, min=-clamp_value, max=clamp_value)
         hidden_states = self.wo(hidden_states)
